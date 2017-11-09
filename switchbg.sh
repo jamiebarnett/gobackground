a2f2e4e178d35f6495e5d8e36cdc893808bc577e
Jamie Barnett <jamie.barnett@coull.com>
2017-11-09 14:17:09 +0000

    changed filename save

--- switchbg.sh
+++ switchbg.sh
@@ -1,3 +1,4 @@
 export IMG=$HOME/go/src/github.com/jamiebarnett/gobackground/img/
+echo $IMG
 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$IMG/octopus.jpg'" && killall Dock 
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$IMG/beautiful.jpg'" && killall Dock
