456a919ffe5fcf545e7ed09ec1c225b81dabdbd3
Jamie Barnett <jamie.barnett@coull.com>
2017-11-15 17:23:48 +0000

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gobackground
+
+Works on OSX High Sierra. 
+
+Will pull in the top rated background images on r/wallpaper, select one  at random and then apply it as dekstop background.
+
--- switchbg.sh
+++
@@ -1,4 +0,0 @@
-export IMG=$HOME/go/src/github.com/jamiebarnett/gobackground/img/
-echo $IMG
-
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$IMG/beautiful.jpg'" && killall Dock
